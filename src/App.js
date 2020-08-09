e5231a15a3d93365bb87048dc5b26cf1999514d2
saripudin15 <sarippudin1511@gmail.com>
2020-08-09 10:08:29 +0700

    setup blog react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Navigation from './components/navigation';
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import PageRenderer from './page-renderer';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navigation></Navigation>
+     <Switch>
+       <Route path="/:page" component={PageRenderer} />
+       <Route path="/" render={() => <Redirect to="/home" /> } />
+       <Route component={() => 404} />
+     </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/navigation.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const navLinks = [
+    {
+        title: 'Home',
+        path: '/'
+    },
+    {
+        title: 'Blog',
+        path: '/blog'
+    },
+    {
+        title: 'Contact',
+        path: '/contact-us'
+    },
+    {
+        title: 'Login',
+        path: '/login'
+    }
+]
+
+export default function Navigation() {
+        return (
+            <nav className="site-navigation">
+                <span>Pujafa</span>
+                <ul>
+                    { navLinks.map((link, index) => (
+                        <li key={index}>
+                            <Link to={link.path}>{link.title}</Link>
+                        </li>
+                    ))   
+                    }
+                </ul>
+            </nav>
+        )
+}
